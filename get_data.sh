846a06e7623f67a806612040f4fe069472dff12e
Jongbin Jung <jongbin@stanford.edu>
2018-09-21 11:54:57 -0700

    Initial data collection script

---
+++ get_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Created: 2018-09-09
+# Author: jongbin
+# Description: Download RData file for SQF analysis
+
+curl --create-dirs https://5harad.com/data/sqf.RData -o data/sqf/sqf.RData
